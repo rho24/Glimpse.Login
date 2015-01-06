ddf6ddb21c3c941da8d1f5e64e79a991a342fd06
Richard Oliver <richard@rholiver.co.uk>
2015-01-06 11:40:58 +0000

    Added basic classes

---
+++ Glimpse.Login/FormHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Web;
+
+namespace Glimpse.Login
+{
+    public class FormHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            using (
+                var file = Assembly.GetExecutingAssembly()
+                        .GetManifestResourceStream("Glimpse.Login.form.html"))
+            {
+                context.Response.ContentType = "text/html";
+                file.CopyTo(context.Response.OutputStream);
+            }
+        }
+
+        public bool IsReusable { get { return true; } }
+    }
+}
---
+++ Glimpse.Login/LoginPolicy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.Login
+{
+    public class LoginPolicy : IRuntimePolicy
+    {
+        private const string ControlCookieName = "Glimpse.Login";
+        private const string AppSettingKey = "Glimpse.Login.Password";
+
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            var cookie = policyContext.RequestMetadata.GetCookie(ControlCookieName);
+
+            if (string.IsNullOrEmpty(cookie))
+            {
+                return RuntimePolicy.Off;
+            }
+
+            if (CredentialsAreValid(cookie))
+                return RuntimePolicy.On;
+
+            return RuntimePolicy.Off;
+        }
+
+        private bool CredentialsAreValid(string password)
+        {
+            var expectedPassword = ConfigurationManager.AppSettings[AppSettingKey];
+
+            if (expectedPassword == null)
+                return false;
+
+            return password.Equals(expectedPassword, StringComparison.InvariantCulture);
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.BeginRequest; }
+        }
+    }
+}
